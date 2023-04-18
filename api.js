169bfb593755f0bf48d04f18d82a47293b4e433d
Martin <pezcornioweb@gmail.com>
2023-04-18 11:43:30 -0300

    Funcionamiento de la api mejorado. se carga la imagen random, falta resto de los dato.

--- api.js
+++ api.js
@@ -1,13 +1,42 @@
-
+$(document).ready(function () {
 
 $.ajax({
-    type: "GET",
-    url: 'https://randomuser.me/api/?inc=gender,name,nat,picture',
-    dataType: 'json',
-    success: function(data) {
-        $.each(data, function(gender,name,nat,picture){
-            console.log(gender, name, nat, picture);
-            $("#comenta1_nombre").inner(name);
-        }
-)}
-});
+    method: "GET",
+    url: 'https://randomuser.me/api/',
+}).done(function (data) {
+    
+    data.results.forEach(usuario => {
+        let firstName = new Object(usuario.name.first); //usuario.name.last, usuario.nat, 
+        let pais = new Object(usuario.nat);
+        let apellido = new Object(usuario.name.last);
+        let foto = new Object(usuario.picture.medium);
+        console.log(firstName, pais, foto, apellido);
+        $("#comenta1_foto").attr("src", foto);
+        $("#comenta1_nombre").append('<h4 class="card-title" id="comenta1_nombre">' + ($(firstName)).val() + '<h4>');
+    });
+    
+    })
+        
+
+            
+        
+     
+    
+  });
+  $(document).ready(function () {
+
+function verDatos(usuarioRandom) {
+    $.ajax({
+        method: "GET",
+        url: usuarioRandom
+    }).done(function (datosUsuarioRandom) {
+        $("#comenta1_card").show();
+
+        $("#comenta1_foto").attr("src", datosUsuarioRandom.picture.thumbnail);
+
+        $("#comenta1_nombre").html(datosUsuarioRandom.name.first);
+console.log(datosUsuarioRandom);
+
+    })
+}
+})
